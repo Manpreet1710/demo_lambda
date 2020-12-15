d1ca9749b037cfd28ff7a48dbdff02ea9657e781
Manpreet singh <ms4110415@gmail.com>
2020-12-15 16:24:23 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+console.log('connected')
+
+let form = document.querySelector('#form')
+console.log(form)
+// let start = document.querySelector('#startsWith')
+
+const data = async (input) => {
+  try {
+    const newdata = await fetch(`http://127.0.0.1:9000/getWords?name=${input}`)
+    const res = await newdata.json()
+    Array.from(res).map((val) => {
+      if (!val.includes(input)) {
+        console.log('not fount')
+      }
+    })
+
+    let ul = document.querySelector('.ul')
+    ul.innerHTML = ''
+    Array.from(res).forEach((elem) => {
+      const li = document.createElement('li')
+      const link = document.createElement('a')
+      link.innerText = elem
+      link.href = '/'
+      li.appendChild(link)
+      ul.appendChild(li)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log('clciked')
+  let input = document.querySelector('.inpt')
+  data(input.value)
+})
